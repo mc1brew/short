d5af277f40482b5888af22ebe3fe2687d5453bc5
mc1brew <1282707+mc1brew@users.noreply.github.com>
2021-02-07 02:05:51 -0700

    Fixing compilation error, fucking semicolons.

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace Short
                     builder =>
                     {
                         builder.WithOrigins(Constants.Configuration.ClientOriginUrl)
-                        .AllowAnyMethod();
+                        .AllowAnyMethod()
                         .AllowAnyHeader();
                     });
             });
