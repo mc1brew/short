84cd5f7fe538721ee4102f57ac2ab3c7516505a9
mc1brew <1282707+mc1brew@users.noreply.github.com>
2021-02-10 17:03:05 -0700

    Added this back to App.js.  In the production build it doesn't take that long to render especially because I can just return null.

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -8,6 +8,23 @@ class App extends Component {
     state = {url:'',name:'', redirectUrl:''}
     render() {
         const {redirectUrl} = this.state
+
+        //Check to see if we should be forwarding somewhere first.
+        var pathname = window.location.pathname;
+        if(pathname != '/')
+        {
+          fetch('/api'+pathname,
+            {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(data => {
+                window.location.replace(data.redirectUrl)
+            });
+
+            return null;
+        }
+
         return (
             <div className="App">
                 <Alert renderResult={this.renderResult} redirectUrl={this.state.redirectUrl} />
